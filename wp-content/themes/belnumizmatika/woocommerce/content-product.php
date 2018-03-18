e96a7f4dd096234f3b90b437c6e552cfa9de9e3b
Artur <tox1ctox@mail.ru>
2018-03-18 14:42:07 +0300

    Changes added

--- wp-content/themes/belnumizmatika/woocommerce/content-product.php
+++ wp-content/themes/belnumizmatika/woocommerce/content-product.php
@@ -53,6 +53,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_product_title - 10
 	 */
         
+        echo 'Изменения видишь?';
 	do_action( 'woocommerce_shop_loop_item_title' );
 
 	/**
