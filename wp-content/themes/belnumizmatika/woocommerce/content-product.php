daa6a55b10a27ad2b715b341f0254cfe0e6cd157
Artur <tox1ctox@mail.ru>
2018-03-18 14:54:08 +0300

    Removed title

--- wp-content/themes/belnumizmatika/woocommerce/content-product.php
+++ wp-content/themes/belnumizmatika/woocommerce/content-product.php
@@ -53,7 +53,6 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_product_title - 10
 	 */
         
-        echo 'Изменения видишь?';
 	do_action( 'woocommerce_shop_loop_item_title' );
 
 	/**
